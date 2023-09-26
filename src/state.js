361a6fcded93ff01a66fb5037067d9e9b6750e1d
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-26 17:22:08 +0200

    Add
    typo in state.js

--- src/state.js
+++ src/state.js
@@ -1,18 +1,19 @@
 import { reactive } from "vue";
-import axios from 'axios';
+import axios from "axios";
 
 export const state = reactive({
-    base_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+    base_url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0 ',
    cards: null,
 
    
         fetchData() {
 
             axios
-                .get(this.base_urlurl)
+                .get(this.base_url)
                 .then(response => {
 
                     this.cards = response.data.data
+                    console.log(this.cards);
 
                 })
                 .catch(error => {
